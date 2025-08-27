15e6060b5aa0aa074df18729887644c17e12d019
cairesh <henriquecaires2004@gmail.com>
2025-08-27 10:08:19 -0400

    atualizando app.py

--- app.py
+++ app.py
@@ -1,31 +1,35 @@
 from flask import Flask
-from .routes.totem import totem
-from .routes.atendente import atendente
-from .routes.painel import painel
-from .database.db import ServicoBancoDeDados
+from routes.totem import totem
+from routes.atendente import atendente
+from routes.painel import painel
+from database.db import ServicoBancoDeDados
 from dotenv import load_dotenv
 from os import getenv
 
-# Carregamento das variaveis de ambiente
-# load_dotenv()
-# host, user, password, database = getenv("HOST"), getenv("USER"), getenv("PASSWORD"), getenv("DATABASE")
+
+# Carregamento das variáveis de ambiente
+load_dotenv()
+host, user, password, database = getenv("HOST"), getenv("USER"), getenv("PASSWORD"), getenv("DATABASE")
+
 
 # Inicialização do banco de dados da aplicação
-# conn = ServicoBancoDeDados.getInstancia()
+conn = ServicoBancoDeDados.getInstancia()
 
-# Inicializa objeto da aplicacao
+
+# Inicializa objeto da aplicação
 app = Flask(__name__)
 
+
 # Registro de blueprints
-
-# Painel
 app.register_blueprint(painel)
-
-# Atendente
 app.register_blueprint(atendente)
-
-# Totem do usuario
 app.register_blueprint(totem)
+
 
 # Buscador para o app dentro do CPanel
 application = app
+
+
+# Execução local da aplicação
+if __name__ == "__main__":
+    app.run(debug=True)
